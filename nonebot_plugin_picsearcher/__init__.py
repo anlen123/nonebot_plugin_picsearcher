1cc4d3a96178c684b466821183cc592ca4f01287
anlen123 <1761512493@qq.com>
2024-07-06 18:48:27 +0800

    fix

--- nonebot_plugin_picsearcher/__init__.py
+++ nonebot_plugin_picsearcher/__init__.py
@@ -6,7 +6,7 @@ import json
 from aiohttp.client_exceptions import ClientError
 
 from nonebot import get_driver
-from nonebot.params import State, ArgPlainText, Arg, CommandArg
+from nonebot.params import ArgPlainText, Arg, CommandArg
 from nonebot.plugin import on_command, on_message
 from nonebot.rule import to_me
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent, GroupMessageEvent, PrivateMessageEvent, Message
@@ -51,122 +51,57 @@ async def get_des(url: str, mode: str):
             yield msg
 
 
-setu = on_command("搜图", aliases={"search"}, rule=to_me())
+setu = on_command("搜图", aliases={"search"})
 
 
 @setu.handle()
-async def handle_first_receive(event: MessageEvent, state: T_State = State(), setu: Message = CommandArg()):
+async def handle_first_receive(event: MessageEvent, state: T_State, setu: Message = CommandArg()):
     if setu:
         state["setu"] = setu
-
-
-@setu.got("mod", prompt="从哪里查找呢? ex/nao/trace/iqdb/ascii2d")
-async def get_func():
-    pass
 
 
 @setu.got("setu", prompt="图呢？")
 async def get_setu(bot: Bot,
                    event: MessageEvent,
-                   mod: str = ArgPlainText("mod"),
                    msg: Message = Arg("setu")):
+    mods = ["sau"]
     """
     发现没有的时候要发问
     :return:
     """
-    try:
-        if msg[0].type == "image":
-            await bot.send(event=event, message="正在处理图片")
-            url = msg[0].data["url"]  # 图片链接
-            if not getattr(bot.config, "risk_control", None) or isinstance(event, PrivateMessageEvent):  # 安全模式
-                async for msg in limiter(get_des(url, mod), getattr(bot.config, "search_limit", None) or 2):
-                    await bot.send(event=event, message=msg)
+    for mod in mods:
+        try:
+            if msg[0].type == "image":
+                await bot.send(event=event, message=f"{mod} 正在处理图片")
+                url = msg[0].data["url"]  # 图片链接
+                if not getattr(bot.config, "risk_control", None) or isinstance(event, PrivateMessageEvent):  # 安全模式
+                    async for msgx in limiter(get_des(url, mod), getattr(bot.config, "search_limit", None) or 2):
+                        await bot.send(event=event, message=msgx)
+                else:
+                    msgs: Message = sum(
+                        [msg if isinstance(msg, Message) else Message(msgx) async for msgx in get_des(url, mod)])
+                    dict_data = json.loads(json.dumps(msgs, cls=DataclassEncoder))
+                    await bot.send_group_forward_msg(group_id=event.group_id,
+                                                    messages=[
+                                                        {
+                                                            "type": "node",
+                                                            "data": {
+                                                                "name": event.sender.nickname,
+                                                                "uin": event.user_id,
+                                                                "content": [
+                                                                    content
+                                                                ]
+                                                            }
+                                                        }
+                                                        for content in dict_data
+                                                    ]
+                                                    )
+
+                # image_data: List[Tuple] = await get_pic_from_url(url)
+                await bot.send(event=event,message=f"{mod}引擎 搜索完毕 hso")
             else:
-                msgs: Message = sum(
-                    [msg if isinstance(msg, Message) else Message(msg) async for msg in get_des(url, mod)])
-                dict_data = json.loads(json.dumps(msgs, cls=DataclassEncoder))
-                await bot.send_group_forward_msg(group_id=event.group_id,
-                                                 messages=[
-                                                     {
-                                                         "type": "node",
-                                                         "data": {
-                                                             "name": event.sender.nickname,
-                                                             "uin": event.user_id,
-                                                             "content": [
-                                                                 content
-                                                             ]
-                                                         }
-                                                     }
-                                                     for content in dict_data
-                                                 ]
-                                                 )
-
-            # image_data: List[Tuple] = await get_pic_from_url(url)
-            await setu.finish("hso")
-        else:
-            await setu.reject("这不是图,重来!")
-    except (IndexError, ClientError):
-        await bot.send(event, traceback.format_exc())
-        await setu.finish("参数错误")
-
-
-pic_map: Dict[str, str] = {}  # 保存这个群的上一张色图 {"123456":"http://xxx"}
-
-
-async def check_pic(bot: Bot, event: MessageEvent, state: T_State = State()) -> bool:
-    if isinstance(event, MessageEvent):
-        for msg in event.message:
-            if msg.type == "image":
-                url: str = msg.data["url"]
-                state["url"] = url
-                return True
-        return False
-
-
-notice_pic = on_message(check_pic, block=False, priority=record_priority)
-
-
-@notice_pic.handle()
-async def handle_pic(event: GroupMessageEvent, state: T_State = State()):
-    try:
-        group_id: str = str(event.group_id)
-        pic_map.update({group_id: state["url"]})
-    except AttributeError:
-        pass
-
-
-previous = on_command("上一张图是什么", aliases={"上一张", "这是什么"})
-
-
-@previous.handle()
-async def handle_previous(bot: Bot, event: GroupMessageEvent):
-    await bot.send(event=event, message="processing...")
-    try:
-        url: str = pic_map[str(event.group_id)]
-        if not getattr(bot.config, "risk_control", None):  # 安全模式
-            async for msg in limiter(get_des(url, "nao"), getattr(bot.config, "search_limit", None) or 2):
-                await bot.send(event=event, message=msg)
-        else:
-            msgs: Message = sum(
-                [msg if isinstance(msg, Message) else Message(msg) async for msg in get_des(url, "nao")])
-            dict_data = json.loads(json.dumps(msgs, cls=DataclassEncoder))
-            await bot.send_group_forward_msg(group_id=event.group_id,
-                                             messages=[
-                                                 {
-                                                     "type": "node",
-                                                     "data": {
-                                                         "name": event.sender.nickname,
-                                                         "uin": event.user_id,
-                                                         "content": [
-                                                             content
-                                                         ]
-                                                     }
-                                                 }
-                                                 for content in dict_data
-                                             ]
-                                             )
-    except (IndexError, ClientError):
-        await bot.send(event, traceback.format_exc())
-        await previous.finish("参数错误")
-    except KeyError:
-        await previous.finish("没有图啊QAQ")
+                await setu.reject("这不是图,重来!")
+        except (IndexError, ClientError):
+            # await bot.send(event, traceback.format_exc())
+            # await setu.finish("参数错误")
+            await bot.send(event=event,message=f"{mod}引擎发生错误")
